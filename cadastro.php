191d5550fa7ab19750e7ce994f457afaa170557d
luizpq <luizfabiano1007@gmail.com>
2024-08-02 15:55:18 -0300

    alguns testes

---
+++ cadastro.php
@@ -0,0 +1,1 @@
+arquivo validado com sucesso
--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-function clickbtn() {
-    var name = document.getElementById('inome')
+ /*function clickbtn() {
+   
+    
     var sobre = document.getElementById('isobre')
     var email = document.getElementById('iemail')
     var query = document.getElementsByName('query')
     var msg = document.getElementById('imsg')
     var check = document.getElementById('iaceitar')
-    var msgerro = document.getElementsByTagName('div.msgerro')
+    var msgerro = document.querySelector('div.msgerro')*/
+    
+    document.addEventListener('DOMContentLoaded', function() {
+        var name = document.getElementById('inome')
+        var form = document.querySelector('form')
+        form.addEventListener('submit', function(event) {
+           
+            if (name.value.trim() == '') {
+                msgerro.innerHTML = `This field is required`
+                event.preventDefault()
+            }       
+        })
+    })
 
-    if (name.value.trim() === '') {
-        msgerro.innerHTML += `This field is required`
-        Event.preventDefault()
-    }
-}
+   
